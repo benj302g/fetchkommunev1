af76dbdb20944c49eb1a13c8566f0bafc70738ae
benj302g <benj302g@stud.kea.dk>
2023-02-24 09:10:32 +0100

    commentar

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,5 +1,7 @@
 console.log("jeg er i dropdown")
 const fruits = []
+
+// vi laver objekter og ligger den ned i arrayet med push
 fruits.push({"name":"æble", "style":"fruit"})
 fruits.push({"name": "pære", "style" : "fruit"})
 fruits.push({"name": "peanut", "style" : "notfruit"})
